2375df6b90951e32d0c20cd20daac7322b62a047
Ozen Burak <burak.ozen6720@gmail.com>
2025-10-22 11:18:37 +0200

    ajout du AuthMiddleware

---
+++ charlyMatLoc/src/api/middlewares/AuthMiddleware.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace charlyMatLoc\src\api\middlewares;
+
+use _PHPStan_90b10482a\Nette\Neon\Exception;
+use Firebase\JWT\BeforeValidException;
+use Firebase\JWT\ExpiredException;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use Firebase\JWT\SignatureInvalidException;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Slim\Exception\HttpUnauthorizedException;
+use charlyMatLoc\src\application_core\application\ports\api\dtos\ProfileDTO;
+
+class AuthMiddleware
+{
+    private string $secretKey;
+
+    public function __construct(string $secretKey)
+    {
+        $this->secretKey = $secretKey;
+    }
+
+    public function __invoke(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        try {
+            $authHeader = $request->getHeaderLine('Authorization');
+            $token = sscanf($authHeader, "Bearer %s")[0] ;
+            $payload = JWT::decode($token, new Key($this->secretKey, 'HS512'));
+        } catch (ExpiredException $e) {
+            throw new Exception("Token expiré");
+        } catch (SignatureInvalidException $e) {
+            throw new Exception("Signature token invalide");
+        } catch (BeforeValidException $e) {
+            throw new Exception("Token pas encore valide");
+        } catch (\UnexpectedValueException $e) {
+            throw new Exception("Valeur non attendu reçu");
+        }
+
+
+        // Créer le profil à partir du token
+        $profile = new ProfileDTO(
+            id: $payload->sub,
+            email: $payload->data->user,
+            role: $payload->data->role
+        );
+
+        // Ajouter le profil dans la requête pour l'action suivante
+        $request = $request->withAttribute('profile', $profile);
+
+        return $handler->handle($request);
+    }
+}
