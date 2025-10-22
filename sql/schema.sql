419494ae3960cbd8b4c77e58c0e0812058bcfd30
Ozen Burak <burak.ozen6720@gmail.com>
2025-10-22 13:07:23 +0200

    modif de la BD

--- sql/schema.sql
+++ sql/schema.sql
@@ -37,7 +37,6 @@ CREATE TABLE "public"."users" (
 "id" uuid NOT NULL,
 "email" character varying(128) NOT NULL,
 "password" character varying(256) NOT NULL,
-"role" smallint DEFAULT '0' NOT NULL,
 CONSTRAINT "users_email" UNIQUE ("email"),
 CONSTRAINT "users_id" PRIMARY KEY ("id")
 ) WITH (oids = false);
