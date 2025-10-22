e657672036f1ff4e71f00d3a380804a0abdc938a
Ozen Burak <burak.ozen6720@gmail.com>
2025-10-22 11:15:50 +0200

    ajout des CredentialsDTO

---
+++ charlyMatLoc/src/application_core/application/ports/api/dtos/CredentialsDTO.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace charlyMatLoc\src\application_core\application\ports\api\dtos;
+
+class CredentialsDTO{
+
+    public function __construct(
+        public  string $email,
+        public  string $password
+    ){}
+
+
+}
