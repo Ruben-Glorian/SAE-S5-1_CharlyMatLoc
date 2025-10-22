e8efceb65032434f57063d308f5111e4c70d1212
Ozen Burak <burak.ozen6720@gmail.com>
2025-10-22 11:19:20 +0200

    ajout du ServiceUser

---
+++ charlyMatLoc/src/application_core/application/usecases/ServiceUser.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace charlyMatLoc\src\application_core\application\usecases;
+
+
+use charlyMatLoc\src\application_core\application\ports\api\dtos\CredentialsDTO;
+use charlyMatLoc\src\application_core\application\ports\api\dtos\ProfileDTO;
+use charlyMatLoc\src\application_core\application\ports\api\ServiceUserInterface;
+use charlyMatLoc\src\application_core\application\ports\spi\AuthRepositoryInterface;
+
+class ServiceUser implements ServiceUserInterface {
+
+    private AuthRepositoryInterface $userRepository;
+
+    public function __construct(AuthRepositoryInterface $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function register(CredentialsDTO $credentials, int $role): ProfileDTO
+    {
+
+        $this->userRepository->save($credentials, $role);
+        $user = $this->userRepository->findByEmail($credentials->email);
+
+        return new ProfileDTO(
+            $user->getId(),
+            $user->getEmail(),
+            $user->getRole()
+        );
+    }
+
+    public function byCredentials(CredentialsDTO $credentials): ?ProfileDTO{
+        $user = $this->userRepository->findByEmail($credentials->email);
+        if($user === null){
+            throw new \Exception("Email éroné");
+        }
+
+        if(!password_verify($credentials->password, $user->getPassword())){
+            throw new \Exception("Mot de passe éroné");
+        }
+
+        return new ProfileDTO(
+            $user->getId(),
+            $user->getEmail(),
+            $user->getRole()
+        );
+    }
+}
