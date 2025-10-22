54265c78aa658ac360f55be112a5ea6273ece87a
Ozen Burak <burak.ozen6720@gmail.com>
2025-10-22 11:17:32 +0200

    ajout des jetons JWT

---
+++ charlyMatLoc/src/api/providers/JWTAuthnProvider.php
@@ -0,0 +1,38 @@
+<?php
+namespace charlyMatLoc\src\api\providers;
+
+use charlyMatLoc\src\application_core\application\ports\api\dtos\AuthDTO;
+use charlyMatLoc\src\application_core\application\ports\api\dtos\CredentialsDTO;
+use charlyMatLoc\src\application_core\application\ports\api\dtos\ProfileDTO;
+use charlyMatLoc\src\application_core\application\ports\api\ServiceUserInterface;
+use Firebase\JWT\JWT;
+
+class JWTAuthnProvider implements AuthnProviderInterface{
+
+    private ServiceUserInterface $serviceUser;
+    private JWTManager $JWTManager;
+
+    public function __construct(JWTManager $jwtManager, ServiceUserInterface $serviceUser){
+        $this->JWTManager = $jwtManager;
+        $this->serviceUser = $serviceUser;
+    }
+
+    public function signin(CredentialsDTO $credentials): array
+    {
+        $user = $this->serviceUser->byCredentials($credentials);
+        $payload = [
+            'iss' => 'http://charlyMatLoc',
+            'iat' => time(),
+            'exp' => time()+3600,
+            'sub' => $user->id,
+            'data' => [
+                'role' => $user->role,
+                'user' => $user->email
+            ]
+        ];
+        $accessToken  = $this->JWTManager->createAccesToken($payload);
+        $refreshToken = $this->JWTManager->createRefreshToken($payload);
+
+        return [new AuthDTO($accessToken, $refreshToken), new ProfileDTO($user->id,$user->email,$user->role)];
+    }
+}
