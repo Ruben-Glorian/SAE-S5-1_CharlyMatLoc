d84effd123ef818d93041015f4404102cb9232a1
Ozen Burak <burak.ozen6720@gmail.com>
2025-10-22 11:16:50 +0200

    ajout du ProfileDTO

---
+++ charlyMatLoc/src/application_core/application/ports/api/dtos/ProfileDTO.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace charlyMatLoc\src\application_core\application\ports\api\dtos;
+
+class ProfileDTO
+{
+    public function __construct(
+        public readonly string $id,
+        public readonly string $email,
+        public readonly int $role
+    ) {
+    }
+}
+
+
